13499fdb3a9014872784169b82732e666bacb27a
denuen <anto.pintauro1@gmail.com>
2025-07-01 19:00:46 +0200

    feat: added core PID module with configurable gains and stable discrete control

---
+++ includes/PID.hpp
@@ -0,0 +1,39 @@
+#ifndef PID_HPP
+#define PID_HPP
+
+#include "Vector3f.hpp"
+
+class PID {
+
+	private:
+		float	kp;
+		float	ki;
+		float	kd;
+		float	integral;
+		float	previousError;
+
+	public:
+		PID();
+		PID(float kp, float ki, float kd);
+		PID(const PID& pid);
+
+		PID&	operator=(const PID& pid);
+
+		inline const float&	getKp(void) const { return (kp); }
+		inline const float&	getKi(void) const { return (ki); }
+		inline const float& getKd(void) const { return (kd); }
+		inline const float& getIntegral(void) const { return (integral); }
+		inline const float& getPrevErr(void) const { return (previousError); }
+
+		void				setKp(const float kp);
+		void				setKi(const float ki);
+		void				setKd(const float kd);
+		void				setGains(const float kp, const float ki, const float kd);
+
+		float				compute(const float setpoint, const float measure, const float dt);
+		void				reset(void);
+
+		~PID();
+};
+
+#endif
---
+++ src/PID.cpp
@@ -0,0 +1,111 @@
+#include "PID.hpp"
+#include <iostream>
+#include <cassert>
+
+PID::PID() :
+kp(0.0f), ki(0.0f), kd(0.0f), integral(0.0f), previousError(0.0f) {
+
+}
+
+PID::PID(const float kp, const float ki, const float kd) :
+kp(0.0f), ki(0.0f), kd(0.0f), integral(0.0f), previousError(0.0f) {
+
+	assert(!std::isinf(kp) && !std::isnan(kp)
+		&& !std::isinf(ki) && !std::isnan(ki)
+		&& !std::isinf(kd) && !std::isnan(kd)
+		&& "Error: PID constructor with arguments: gain values must be finite");
+
+	this->kp = kp;
+	this->ki = ki;
+	this->kd = kd;
+
+}
+
+PID::PID(const PID& pid) {
+
+	kp = pid.kp;
+	ki = pid.ki;
+	kd = pid.kd;
+	integral = pid.integral;
+	previousError = pid.previousError;
+
+}
+
+PID&	PID::operator=(const PID& pid) {
+
+	if (this != &pid) {
+		kp = pid.kp;
+		ki = pid.ki;
+		kd = pid.kd;
+		integral = pid.integral;
+		previousError = pid.previousError;
+	}
+
+	return (*this);
+}
+void	PID::setKp(const float kp) {
+
+	assert(!std::isinf(kp) && !std::isnan(kp) && "Error: PID setKp: value must be finite");
+	this->kp = kp;
+
+}
+
+void	PID::setKi(const float ki) {
+
+	assert(!std::isinf(ki) && !std::isnan(ki) && "Error: PID setKi: value must be finite");
+	this->ki = ki;
+
+}
+
+void	PID::setKd(const float kd) {
+
+	assert(!std::isinf(kd) && !std::isnan(kd) && "Error: PID setKd: value must be finite");
+	this->kd = kd;
+
+}
+
+void	PID::setGains(const float kp, const float ki, const float kd) {
+
+		assert(!std::isinf(kp) && !std::isnan(kp)
+		&& !std::isinf(ki) && !std::isnan(ki)
+		&& !std::isinf(kd) && !std::isnan(kd)
+		&& "Error: PID constructor with arguments: gain values must be finite");
+
+		this->kp = kp;
+		this->ki = ki;
+		this->kd = kd;
+
+}
+
+float	PID::compute(const float setpoint, const float measure, const float dt) {
+	float	ek;
+	float	derivative;
+	float	result;
+
+	assert(dt > 0.0f && "Error: dt must be positive");
+
+	ek = setpoint - measure;
+	assert(!std::isinf(ek) && !std::isnan(ek) && "Error: the error at the k-iteration must be finite");
+
+	integral += ek * dt;
+	derivative = (ek - previousError) / dt;
+
+	result = kp * ek + ki * integral + kd * derivative;
+	previousError = ek;
+
+	assert(!std::isinf(result) && !std::isnan(result) && "Error: PID output invalid");
+	return (result);
+
+}
+
+void	PID::reset(void) {
+
+	integral = 0.0f;
+	previousError = 0.0f;
+
+}
+
+
+PID::~PID() {
+
+}

39955767feb7590bd77843fddffab280f89aa46f
denuen <anto.pintauro1@gmail.com>
2025-07-01 19:15:28 +0200

    feat: added check variables to the PID module to ensure a not-saturated execution during the integral and output calculus

--- includes/PID.hpp
+++ includes/PID.hpp
@@ -12,6 +12,11 @@ class PID {
 		float	integral;
 		float	previousError;
 
+		static const float	DEFAULT_INTEGRAL_MIN;
+		static const float	DEFAULT_INTEGRAL_MAX;
+		static const float	DEFAULT_OUTPUT_MIN;
+		static const float	DEFAULT_OUTPUT_MAX;
+		
 	public:
 		PID();
 		PID(float kp, float ki, float kd);
@@ -19,11 +24,11 @@ class PID {
 
 		PID&	operator=(const PID& pid);
 
-		inline const float&	getKp(void) const { return (kp); }
-		inline const float&	getKi(void) const { return (ki); }
-		inline const float& getKd(void) const { return (kd); }
-		inline const float& getIntegral(void) const { return (integral); }
-		inline const float& getPrevErr(void) const { return (previousError); }
+		inline const float&		getKp(void) const { return (kp); }
+		inline const float&		getKi(void) const { return (ki); }
+		inline const float&		getKd(void) const { return (kd); }
+		inline const float& 	getIntegral(void) const { return (integral); }
+		inline const float& 	getPrevErr(void) const { return (previousError); }
 
 		void				setKp(const float kp);
 		void				setKi(const float ki);
